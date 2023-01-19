dfc59e0a5195238de74fb2b32a5c0f88808f6603
newojima <nelsonalbajr@yahoo.com>
2023-01-20 03:28:10 +0900

    Create JwtAuthenticationFilter.java

---
+++ backend/src/main/java/com/nejilabs/backend/config/JwtAuthenticationFilter.java
@@ -0,0 +1,21 @@
+package com.nejilabs.backend.config;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+  @Override
+  protected void doFilterInternal(
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+  }
+}
