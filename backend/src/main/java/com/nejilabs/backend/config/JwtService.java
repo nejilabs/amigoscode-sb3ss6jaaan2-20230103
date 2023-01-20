704e51ab265af09a723c5e760482cb83eec7c534
newojima <nelsonalbajr@yahoo.com>
2023-01-20 15:54:55 +0900

    Update JwtService.java

--- backend/src/main/java/com/nejilabs/backend/config/JwtService.java
+++ backend/src/main/java/com/nejilabs/backend/config/JwtService.java
@@ -2,9 +2,21 @@ package com.nejilabs.backend.config;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 @Service
 public class JwtService {
   public String extractUsername(String token) {
     return null;
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts
+        .parserBuilder()
+        .setSigningKey(getSignInKey())
+        .build()
+        .parseClaimsJws()
+        .getBody();
   }
 }
